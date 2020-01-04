cf8f520be90571f088bd73ce66e18159b552a029
pedrohf360 <pedro.henriqueff10@gmail.com>
2020-01-04 16:44:02 -0300

    Parsing body and handling CORS.

--- node-rest-shop/api/routes/orders.js
+++ node-rest-shop/api/routes/orders.js
@@ -8,8 +8,13 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+    const order = {
+        product: req.body.productId,
+        quantity: req.body.quantity
+    }
     res.status(201).json({
-        message: 'Order was created'
+        message: 'Order was created',
+        order: order
     });
 });
 
